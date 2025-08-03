464af64c305feb4e28d5e5d0d660d696e96949d9
Shubhamsahar <kumarshubham765484@gmail.com>
2025-08-03 16:17:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function printFunctionName(func) {
+    alert(func.name);
+}
+
+function testFunction() {
+    printFunctionName(testFunction);
+}
+
+testFunction();
